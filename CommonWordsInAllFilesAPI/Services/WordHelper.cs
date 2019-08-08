ba42802f2ad01f955b60f52df1b16c401cc391e7
Sameer <reemas743@gmail.com>
2019-08-09 02:28:38 +0530

    Initial Commit

---
+++ CommonWordsInAllFilesAPI/Services/WordHelper.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CommonWordsInAllFilesAPI.Services
+{
+    public class WordHelper
+    {
+        public string InsertWord(Trie root, string word,int fileIndex)
+        {
+            if (string.IsNullOrEmpty(word))
+            {
+                return null;
+            }
+            Trie temp = root;
+            word = word.ToLower();
+            for(int i = 0; i < word.Length; i++)
+            {
+                int idx = CalcIndex(word[i]);
+                if (temp.Children[idx] != null)
+                {
+                    temp = temp.Children[idx];
+                }
+                else
+                {
+                    temp.Children[idx] = new Trie();
+                    temp = temp.Children[idx];
+                }
+            }
+            if (temp.fileIdx == fileIndex - 1)
+            {
+                temp.fileIdx = fileIndex;
+                return word;
+            }
+            return null;
+        }
+        private int CalcIndex(char c)
+        {
+            if(c>='a' && c <= 'z')
+            {
+                return c - 'a'+10;
+            }
+            else
+            {
+                return c - '0';
+            }
+        }
+    }
+
+    public class Trie
+    {
+        public Trie[] Children;
+        public int fileIdx;
+        public Trie()
+        {
+            fileIdx = -1;
+            Children = new Trie[36];
+        }
+    }
+}
---
+++ SelfHostingWithConsole/Program.cs
@@ -0,0 +1,25 @@
+﻿using CommonWordsInAllFilesAPI;
+using CommonWordsInAllFilesAPI.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.SelfHost;
+
+namespace SelfHostingWithConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config= new HttpSelfHostConfiguration("http://localhost:1234");
+            WebApiConfig.Register(config);
+            var server = new HttpSelfHostServer(config);
+            var task = server.OpenAsync();
+            task.Wait();
+            Console.WriteLine("Web API Server has started at http://localhost:1234");
+            Console.ReadLine();
+        }
+    }
+}
